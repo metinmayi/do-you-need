caf0a6d74f07b5f2a633f62a88e02976a3ba5a83
Metin Ahmad <metinmayi@gmail.com>
2022-05-09 20:44:41 +0200

    Created a login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import LoginRegistration from "../components/LoginRegistration";
 
 type Props = {};
 const LoginPage: React.FC<Props> = () => {
-  return <></>;
+  return (
+    <Wrapper>
+      <Title>Do You Need</Title>
+      <LoginRegistration></LoginRegistration>
+    </Wrapper>
+  );
 };
 
 export default LoginPage;
+
+const Title = styled.p`
+  margin: 0;
+  font-size: 3.5rem;
+  font-family: var(--primaryFamily);
+`;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5%;
+  color: var(--foregroundColor);
+`;
