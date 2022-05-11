a9a7499cb72bffc61b39a390ae4fac20f600ceba
Metin Ahmad <metinmayi@gmail.com>
2022-05-12 01:39:26 +0200

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
+import MissingPage from "./pages/MissingPage";
 
 const App: React.FC = () => {
-  // States
-  const [online, setonline] = useState(true);
-  return <>{online && <LoginPage />}</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="*" element={<MissingPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
