6aa609e4f9314e6674a1685b6e818030a7dc2a9f
Metin Ahmad <metinmayi@gmail.com>
2022-05-28 13:00:44 +0200

    Setup mock data for bossPage

--- src/pages/BossPage/index.tsx
+++ src/pages/BossPage/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { Header } from "../../components/Header";
+import { Table } from "../../components/Table";
+import { BossData } from "../../models/BossData";
 import { PageWrapper } from "./pageWrapper";
 
 const BossPage: React.FC = () => {
+  // Mock boss data
+  const bossData: BossData = {
+    bossItems: {},
+    players: [
+      {
+        name: "Maestro",
+        role: "tank",
+        twoHand: "3.6%,200",
+        gloves: "0",
+        legs: "1%,100",
+        shield: "0",
+        trinket: "8%, 340",
+        feet: "0",
+      },
+    ],
+  };
   return (
     <PageWrapper>
       <Header></Header>
+      <Table data={bossData}></Table>
     </PageWrapper>
   );
 };
