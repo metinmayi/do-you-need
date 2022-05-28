43f46f6db5c7d59c317b327dc86da9fe3bca00a7
Metin Ahmad <metinmayi@gmail.com>
2022-05-28 13:00:04 +0200

    Created module for Player

---
+++ src/models/Player.tsx
@@ -0,0 +1,20 @@
+export interface Player {
+  name: string;
+  role: "tank";
+  head?: string;
+  neck?: string;
+  shoulders?: string;
+  back?: string;
+  chest?: string;
+  wrist?: string;
+  gloves?: string;
+  waist?: string;
+  legs?: string;
+  feet?: string;
+  ring?: string;
+  trinket?: string;
+  oneHand?: string;
+  twoHand?: string;
+  offHand?: string;
+  shield?: string;
+}
