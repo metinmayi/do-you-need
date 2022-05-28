77870cd24368f73e1ecaeb8c6b52ee3cf8866107
Metin Ahmad <metinmayi@gmail.com>
2022-05-28 12:03:52 +0200

    Form Styling

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -12,11 +12,12 @@ export { Form };
 
 export const Container = styled.form`
   width: min(50vw, 600px);
-  padding: 2% 0;
+  padding: 1% 0;
   border: 1px solid #c4c4c4;
   border-radius: 5%;
   display: flex;
   flex-direction: column;
+  gap: 5px;
   align-items: center;
   color: white;
   font-family: var(--primaryFamily);
