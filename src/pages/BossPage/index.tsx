9af6e71abae902f54a3b7aa1d6a7c265cd97f7ed
Metin Ahmad <metinmayi@gmail.com>
2022-05-28 12:04:26 +0200

    Created bossPage

--- src/pages/BossPage/index.tsx
+++ src/pages/BossPage/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Header } from "../../components/Header";
 import { PageWrapper } from "./pageWrapper";
 
 const BossPage: React.FC = () => {
-  return <PageWrapper></PageWrapper>;
+  return (
+    <PageWrapper>
+      <Header></Header>
+    </PageWrapper>
+  );
 };
 
 export { BossPage };
