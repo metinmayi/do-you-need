0e8ffdc90b2a322a737ae7c266b48b8d9ac7603c
Metin Ahmad <metinmayi@gmail.com>
2022-06-13 23:45:28 +0200

    Created A Modal Component

---
+++ src/components/ModalComponent/ModalComponent.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+type ModalComponentProps = {
+  title?: string;
+  Component: React.FC;
+  handleClick?: () => void;
+};
+const ModalComponent: React.FC<ModalComponentProps> = ({
+  Component,
+  title,
+  handleClick,
+}) => {
+  return (
+    <Modal show={true} className="text-secondary">
+      {title && (
+        <Modal.Header>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+      )}
+      <Modal.Body>
+        <Component></Component>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={() => handleClick && handleClick()}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export { ModalComponent };
