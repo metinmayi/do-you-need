403bec003cbaa2e5ce3748f0efd73b7004528b9b
Metin Ahmad <metinmayi@gmail.com>
2022-06-13 16:01:40 +0200

    Added some responsivity to LandingPage

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,11 +1,8 @@
-
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import RosterBackgroundImage from "../../assets/images/Roster.jpg";
 import UpgradesBackgroundImage from "../../assets/images/Upgrades.jpg";
 import { ImageTitle } from "./ImageTitle";
-import { Section } from "./section";
-import { IndexWrapper } from "./indexWrapper";
 import { Title } from "./title";
 import { useRedirect } from "../../utils/utils";
 
@@ -13,74 +10,58 @@ const LandingPage: React.FC = () => {
   const redirect = useRedirect();
   return (
     <Container fluid className="d-grid gap-5">
-      <Row className="justify-content-center mt-3">
-        <Col xs={5} lg={3} className="d-flex gap-2">
-        <Button className="flex-grow-1 border" variant="secondary" onClick={() => redirect('/login')}>Login</Button>
-        <Button className="flex-grow-1 border" variant="secondary" onClick={() => redirect('/register')}>Register</Button>
+      <Row className="mt-2 justify-content-end">
+        <Col xs={7} lg={3} className="d-flex gap-2">
+          <Button
+            className="flex-grow-1 border"
+            variant="secondary"
+            onClick={() => redirect("/login")}>
+            Login
+          </Button>
+          <Button
+            className="flex-grow-1 border"
+            variant="secondary"
+            onClick={() => redirect("/register")}>
+            Register
+          </Button>
         </Col>
       </Row>
       <Row className="text-center">
-        <Title>Do You Need</Title>
-        <p>Raid Managment Tool</p>
+        <Title className="mb-5">Do You Need</Title>
       </Row>
       <Row className="justify-content-center">
         <Col md={10} xl={7}>
           <ImageTitle image={RosterBackgroundImage}>
-          <h2>Manage Your Roster</h2>
+            <h2>Manage Your Roster</h2>
           </ImageTitle>
           <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
-          ratione aperiam perspiciatis placeat quidem natus ex quas quae
-          reiciendis dicta illo distinctio, odio nesciunt sit nihil itaque nisi
-          ullam. Quam rem distinctio, non dolores dolor cum recusandae sit
-          ratione! Vero blanditiis, voluptatibus dolorem eveniet molestias nam
-          alias reprehenderit, nemo sapiente consequuntur voluptas doloremque
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            ratione aperiam perspiciatis placeat quidem natus ex quas quae
+            reiciendis dicta illo distinctio, odio nesciunt sit nihil itaque
+            nisi ullam. Quam rem distinctio, non dolores dolor cum recusandae
+            sit ratione! Vero blanditiis, voluptatibus dolorem eveniet molestias
+            nam alias reprehenderit, nemo sapiente consequuntur voluptas
+            doloremque
           </p>
         </Col>
       </Row>
       <Row className="justify-content-center">
         <Col md={10} xl={7}>
-          <ImageTitle image={RosterBackgroundImage}>
-          <h2>Track Upgrades</h2>
+          <ImageTitle image={UpgradesBackgroundImage}>
+            <h2>Track Upgrades</h2>
           </ImageTitle>
           <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
-          ratione aperiam perspiciatis placeat quidem natus ex quas quae
-          reiciendis dicta illo distinctio, odio nesciunt sit nihil itaque nisi
-          ullam. Quam rem distinctio, non dolores dolor cum recusandae sit
-          ratione! Vero blanditiis, voluptatibus dolorem eveniet molestias nam
-          alias reprehenderit, nemo sapiente consequuntur voluptas doloremque
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            ratione aperiam perspiciatis placeat quidem natus ex quas quae
+            reiciendis dicta illo distinctio, odio nesciunt sit nihil itaque
+            nisi ullam. Quam rem distinctio, non dolores dolor cum recusandae
+            sit ratione! Vero blanditiis, voluptatibus dolorem eveniet molestias
+            nam alias reprehenderit, nemo sapiente consequuntur voluptas
+            doloremque
           </p>
         </Col>
       </Row>
     </Container>
-
-    // <IndexWrapper>
-    //   </Section>
-    //   <Section direction="column">
-
-
-    //   </Section>
-    //   <Section direction="column">
-    //     <ImageTitle image={UpgradesBackgroundImage}>
-    //       <h2>Track Upgrades</h2>
-    //     </ImageTitle>
-    //     <p>
-    //       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
-    //       expedita eveniet beatae minima minus eaque quam fuga dolorem cum
-    //       dignissimos. Tenetur quasi aliquam aspernatur aliquid possimus laborum
-    //       quaerat a inventore eveniet, ipsum aperiam! Voluptatem ullam minus
-    //       sunt recusandae, sint blanditiis eius dolorum optio soluta
-    //       necessitatibus vero, praesentium aperiam laboriosam ipsa. Amet rerum
-    //       necessitatibus culpa quisquam soluta possimus harum nesciunt modi
-    //       nulla tempore quis optio ratione nostrum id libero omnis porro nisi
-    //       unde ad, incidunt consequuntur ipsam. Reprehenderit nobis pariatur
-    //       maxime nam minus cupiditate, soluta voluptatibus laborum, in rerum
-    //       vitae ullam consequuntur tempore suscipit quas, repellendus quia
-    //       dolores saepe expedita tenetur!
-    //     </p>
-    //   </Section>
-    // </IndexWrapper>
   );
 };
 
--- src/pages/LandingPage/title.tsx
+++ src/pages/LandingPage/title.tsx
@@ -4,4 +4,7 @@ export const Title = styled.p`
   margin: 0;
   font-size: 3.5rem;
   font-family: var(--primaryFamily);
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
 `;
