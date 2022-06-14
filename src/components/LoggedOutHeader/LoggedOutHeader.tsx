23f8be2caf629878f9189032a00f4a48a85269b3
Metin Ahmad <metinmayi@gmail.com>
2022-06-15 00:15:51 +0200

    Made LoggedOutHeader responsive

--- src/components/LoggedOutHeader/LoggedOutHeader.tsx
+++ src/components/LoggedOutHeader/LoggedOutHeader.tsx
@@ -5,7 +5,7 @@ import Logo from "../../assets/images/Lootbag.png";
 const LoggedOutHeader: React.FC = () => {
   return (
     <Row className="d-flex justify-content-center mt-5">
-      <Col xs={2}>
+      <Col xs={8} sm={6} md={4} lg={2}>
         <img src={Logo} width="100%" height="100%"></img>
       </Col>
     </Row>
