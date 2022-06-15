0280d05351602023ef9779c4e14462c9efe01233
Metin Ahmad <metinmayi@gmail.com>
2022-06-15 17:05:55 +0200

    Added useMediaQuery hook

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const useRedirect = () => {
   return useNavigate();
+};
+
+/**
+ *
+ * @param query Pixels to compare against
+ * @returns True if smaller, false if larger
+ */
+const useMediaQuery = (query: number) => {
+  const [isDesktop, setDesktop] = useState(window.innerWidth <= query);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth <= query);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, [window.innerWidth]);
+  return isDesktop;
 };
 
 /**
@@ -11,4 +31,4 @@ const hasVisitedBefore = () => {
   return localStorage.getItem("hasVisited") ? true : false;
 };
 
-export { useRedirect, hasVisitedBefore };
+export { useRedirect, hasVisitedBefore, useMediaQuery };
