486030d3368bc1dea889a9ecdc2457e1c7e598a0
Metin Ahmad <metinmayi@gmail.com>
2022-06-20 22:18:02 +0200

    Bossdata is now fixed

---
+++ src/models/Bosses.ts
@@ -0,0 +1,16 @@
+const VigilantGuardian = [
+  "selected",
+  "name",
+  "role",
+  "back",
+  "neck",
+  "ring",
+  "chest",
+  "feet",
+  "hands",
+  "legs",
+  "shoulder",
+  "waist",
+] as const;
+
+export { VigilantGuardian };
--- src/models/DefaultBossData.tsx
+++
@@ -1,1 +0,0 @@
-export type DefaultBossData = "selected" | "name" | "role";
