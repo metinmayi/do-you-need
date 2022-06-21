9cca2728d6f82ee3aa41da310821b8a57e687951
Metin Ahmad <metinmayi@gmail.com>
2022-06-21 18:47:53 +0200

    Made a TableComponent instead of inline

--- src/pages/BossPage/BossPage.tsx
+++ src/pages/BossPage/BossPage.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Table, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { DesktopHeader } from "../../components/DesktopHeader";
 import { MobileHeader } from "../../components/MobileHeader";
 import { useMediaQuery } from "../../customHooks/customHooks";
 import { VigilantGuardian } from "../../models/Bosses";
-import { TableHead } from "./TableHead";
 import { HeaderData } from "../../models/HeaderData";
-import { TableBody } from "./TableBody";
 import { Player } from "../../models/Player";
 import { getPlayers } from "../../api/players";
-import spinner from "../../assets/images/loadingSpinner.svg";
+import { TableComponent } from "./TableComponent";
 
 const BossPage: React.FC = () => {
   // Load Boss
@@ -33,10 +31,11 @@ const BossPage: React.FC = () => {
       {isMobile ? <MobileHeader /> : <DesktopHeader />}
       <Row>
         <Col xs={12} sm={10} className="text-center">
-          {players.length > 0  ?             <Table variant="dark" striped hover responsive>
-              <TableHead headerItems={boss}/>
-              <TableBody players={players} setPlayers={setPlayers}/>
-            </Table> : <><img src={spinner} alt="loading..."></img> <h3>Loading Table</h3></>}
+          <TableComponent
+            boss={boss}
+            players={players}
+            setPlayers={setPlayers}
+          />
         </Col>
       </Row>
     </Container>
---
+++ src/pages/BossPage/TableComponent/TableComponent.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+import { HeaderData } from "../../../models/HeaderData";
+import { Player } from "../../../models/Player";
+import { TableBody } from "../TableBody";
+import { TableHead } from "../TableHead";
+import spinner from "../../../assets/images/loadingSpinner.svg";
+
+interface TableComponentProps {
+  boss: HeaderData[];
+  players: Player[];
+  setPlayers: Function;
+}
+const TableComponent: React.FC<TableComponentProps> = ({
+  boss,
+  players,
+  setPlayers,
+}) => {
+  return players.length > 0 ? (
+    <Table variant="dark" striped hover responsive>
+      <TableHead headerItems={boss} />
+      <TableBody players={players} setPlayers={setPlayers} />
+    </Table>
+  ) : (
+    <>
+      <Table variant="dark" striped hover responsive>
+        <TableHead headerItems={boss} />
+      </Table>
+      <img src={spinner} alt="loading..."></img> <h3>Loading Table</h3>
+    </>
+  );
+};
+
+export { TableComponent };
