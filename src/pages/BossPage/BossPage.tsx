6d761de1d16810b3926866a53b8195b7c297c87c
Metin Ahmad <metin.ahmad@paligo.net>
2022-06-21 08:31:49 +0200

    Moved logic from BossPage to TableBody

--- src/pages/BossPage/BossPage.tsx
+++ src/pages/BossPage/BossPage.tsx
@@ -1,29 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Col, Container, Table, Row } from "react-bootstrap";
 import { DesktopHeader } from "../../components/DesktopHeader";
 import { MobileHeader } from "../../components/MobileHeader";
-import { Player } from "../../models/Player";
 import { useMediaQuery } from "../../customHooks/customHooks";
-import TableRow from "./TableRow/TableRow";
 import { VigilantGuardian } from "../../models/Bosses";
-import { getPlayers } from "../../api/players";
-import Spinner from "../../assets/images/loadingSpinner.svg";
 import { TableHead } from "./TableHead";
 import { HeaderData } from "../../models/HeaderData";
+import { TableBody } from "./TableBody";
 
 const BossPage: React.FC = () => {
   // Load Boss
   const boss: HeaderData[] = [...VigilantGuardian];
-
-  // Fetch players - MOCK atm
-  const [players, setPlayers] = useState<Player[]>([]);
-  useEffect(() => {
-    const fetchPlayers = async () => {
-      const fetchedPlayers: Player[] = await getPlayers(2);
-      setPlayers(fetchedPlayers);
-    };
-    fetchPlayers();
-  }, []);
 
   // Checks if mobile
   const isMobile = useMediaQuery(900);
@@ -33,22 +20,10 @@ const BossPage: React.FC = () => {
       {isMobile ? <MobileHeader /> : <DesktopHeader />}
       <Row>
         <Col xs={12} sm={10}>
-          {players.length > 0 ? (
             <Table variant="dark" striped hover responsive>
               <TableHead headerItems={boss}/>
-              <tbody>
-                {players.map((_, index) => (
-                  <TableRow
-                    players={players}
-                    key={players[index].id}
-                    index={index}
-                    setPlayers={setPlayers}></TableRow>
-                ))}
-              </tbody>
+              <TableBody/>
             </Table>
-          ) : (
-            <img src={Spinner} alt="Loading..."></img>
-          )}
         </Col>
       </Row>
     </Container>
