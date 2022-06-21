858fb231bf27dd545098f14a4c80edccdde87ffa
Metin Ahmad <metin.ahmad@paligo.net>
2022-06-21 17:09:55 +0200

    Moved state up from TableBody to BossPage

--- src/pages/BossPage/BossPage.tsx
+++ src/pages/BossPage/BossPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Table, Row } from "react-bootstrap";
 import { DesktopHeader } from "../../components/DesktopHeader";
 import { MobileHeader } from "../../components/MobileHeader";
@@ -7,10 +7,22 @@ import { VigilantGuardian } from "../../models/Bosses";
 import { TableHead } from "./TableHead";
 import { HeaderData } from "../../models/HeaderData";
 import { TableBody } from "./TableBody";
+import { Player } from "../../models/Player";
+import { getPlayers } from "../../api/players";
 
 const BossPage: React.FC = () => {
   // Load Boss
   const boss: HeaderData[] = [...VigilantGuardian];
+
+  // Initiated the players state.
+  const [players, setPlayers] = useState<Player[]>([]);
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      const fetchedPlayers: Player[] = await getPlayers();
+      setPlayers(fetchedPlayers);
+    };
+    fetchPlayers();
+  }, []);
 
   // Checks if mobile
   const isMobile = useMediaQuery(900);
@@ -22,7 +34,7 @@ const BossPage: React.FC = () => {
         <Col xs={12} sm={10}>
             <Table variant="dark" striped hover responsive>
               <TableHead headerItems={boss}/>
-              <TableBody/>
+              <TableBody players={players} setPlayers={setPlayers}/>
             </Table>
         </Col>
       </Row>
--- src/pages/BossPage/TableBody/TableBody.tsx
+++ src/pages/BossPage/TableBody/TableBody.tsx
@@ -1,21 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { getPlayers } from "../../../api/players";
+import React from "react";
 import { Player } from "../../../models/Player";
 import { PlayerRow } from "./PlayerRow";
 
-
-const TableBody:React.FC = () => {
-  // Fetch players - MOCK atm
-  const [players, setPlayers] = useState<Player[]>([]);
-  useEffect(() => {
-    const fetchPlayers = async () => {
-      const fetchedPlayers: Player[] = await getPlayers();
-      setPlayers(fetchedPlayers);
-    };
-    fetchPlayers();
-  }, []);
+interface TableBodyProps {
+  players: Player[];
+  setPlayers: Function;
+}
+const TableBody:React.FC<TableBodyProps> = ({players, setPlayers}) => {
     return <tbody>
-      {players.map((player, index) => <PlayerRow players={players} playerIndex={index} setPlayers={setPlayers} key={index}/>)}
+      {players.map((_, index) => <PlayerRow players={players} playerIndex={index} setPlayers={setPlayers} key={index}/>)}
     </tbody>
 }
 
