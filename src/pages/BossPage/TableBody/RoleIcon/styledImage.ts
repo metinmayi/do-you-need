4918c0b27307e3ce517807a883e64c79e98fbdb5
Metin Ahmad <metinmayi@gmail.com>
2022-06-21 18:34:45 +0200

    Made RoleIcons responsive

--- src/pages/BossPage/TableBody/RoleIcon/styledImage.ts
+++ src/pages/BossPage/TableBody/RoleIcon/styledImage.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 const StyledImage = styled.img`
-width: 50%;
+width: 30%;
 height: 25%;
-object-fit: cover;`
+object-fit: cover;
+@media screen and (max-width: 1470px) {
+    width: 80%;
+  }
+`
 
 
 export {StyledImage}
