aeb868ba458d28695bbdb168bbd5f1f824067d6d
Metin Ahmad <metin.ahmad@paligo.net>
2022-06-21 08:53:29 +0200

    Players are now a state

--- src/pages/BossPage/TableBody/PlayerRow/PlayerRow.tsx
+++ src/pages/BossPage/TableBody/PlayerRow/PlayerRow.tsx
@@ -11,9 +11,16 @@ const PlayerRow: React.FC<PlayerRowProps> = ({ players, playerIndex, setPlayers 
   // Breaks the player object into an array
   const player: Player = players[playerIndex];
   const iterablePlayer = getIterablePlayer(player);
+  
+  // Function for updating players state to reflect the "selected" values.
+  const toggleSelected = (playerIndex:number) => {
+    const newPlayers = [...players];
+    newPlayers[playerIndex].selected = !newPlayers[playerIndex].selected;
+    setPlayers(newPlayers);
+  }
   return (
     <tr>
-      <td> <input type="checkbox" /></td>
+      <td> <input type="checkbox" checked={player.selected} onClick={() => toggleSelected(playerIndex)} onChange={() => console.log('changed')}/></td>
       {iterablePlayer.map(([key, value]) => <td key={key}>{value}</td>)}
     </tr>
   );
