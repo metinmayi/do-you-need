a6a8c7378cfcb28ae51adb5c4fd6716be9e0a33f
Metin Ahmad <metin.ahmad@paligo.net>
2022-07-31 12:40:15 +0200

    Styling MobileHeader

--- src/components/MobileHeader/MobileHeader.tsx
+++ src/components/MobileHeader/MobileHeader.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
-import { Title } from "../DesktopHeader/title";
 import Logo from "../../assets/images/Lootbag.png";
 import { DropdownComponent } from "../DropdownComponent/DropdownComponent";
 import { BOSSES, BOSSNAMES } from "../../models/Bosses";
@@ -15,15 +14,15 @@ const MobileHeader: React.FC = () => {
     }
   };
   return (
-    <Row className="d-flex justify-content-center p-1">
+    <Row className="d-flex p-1">
       <Col
         xs={4}
         sm={3}
         className="d-flex align-items-center justify-content-end">
         <img src={Logo} alt="Not found" style={{ width: "90%" }} />
       </Col>
-      <Col className="d-flex justify-content-end">
-        <Title> {currentBoss}</Title>
+      <Col className="d-flex justify-content-end align-items-center">
+        <h1> {currentBoss}</h1>
       </Col>
       <Col className="d-flex justify-content-end align-items-center gap-1">
         <DropdownComponent
