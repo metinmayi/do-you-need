9dbd1b62d3adcec918b9dd954267ab48a7fd0dfa
Metin Ahmad <metinmayi@gmail.com>
2022-08-12 01:02:46 +0200

    Added config

---
+++ src/config/config.ts
@@ -0,0 +1,9 @@
+/**
+ * Toggles DEMO vs prod
+ */
+export const ENVIRONMENT: "production" | "demo" = "production" as const;
+
+/**
+ * Sets the domain for the project
+ */
+export const BASE_URL = "http://localhost:8000/" as const;
