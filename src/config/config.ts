4c45587b6574712753bf9e07470a1762fa9d4c81
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-19 23:51:59 +0200

    Added endpoints to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,3 +7,18 @@ export const ENVIRONMENT: "production" | "demo" = "production" as const;
  * Sets the domain for the project
  */
 export const BASE_URL = "http://localhost:8000/" as const;
+
+/**
+ * API Endpoint for checking if user has a valid token registered to them
+ */
+export const VALID_TOKEN_URL = `${BASE_URL}blizzard/checkAccessToken`;
+
+/**
+ * API for authenticating with blizzard. I.E get forwarded to their website
+ */
+export const BLIZZARD_AUTH_URL = `${BASE_URL}blizzard/authenticate`
+
+/**
+ * API for getting all max level chars belonging to this account
+ */
+export const GET_CHARS_URL = `${BASE_URL}blizzard/getCharacters`;
