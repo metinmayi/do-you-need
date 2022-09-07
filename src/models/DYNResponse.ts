cb11b190067a060e2c4bd6a2ccb7127622b81ccd
Metin Ahmad <metinmayi@gmail.com>
2022-09-08 00:02:19 +0200

    Added response object from the API

---
+++ src/models/DYNResponse.ts
@@ -0,0 +1,9 @@
+/**
+ * Expected response object from the server
+ */
+export class DYNResponse {
+  error: boolean = false;
+  message?: string;
+  errorMessage?: string;
+  data: Array<any> = [];
+}
