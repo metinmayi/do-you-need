50af806fd801d0b017c4bb36b0b18d023863fc8a
Metin Ahmad <metinmayi@gmail.com>
2022-09-08 00:27:18 +0200

    Created GuildSelection component

---
+++ src/pages/SynchronizePage/SelectionCard/GuildSelection/GuildSelection.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import { getGuild } from "../../../../api/blizzard.ts/getGuild";
+import { IGuild, isIGuild } from "../../../../models/IGuild";
+import { RetrievedCharacter } from "../../../../models/RetrievedCharacter";
+import { LoadingSpinner } from "../../../../components/LoadingSpinner/LoadingSpinner";
+import {
+  isIUnregisteredGuild,
+  IUnregisteredGuild,
+} from "../../../../models/IUnregisteredGuild";
+
+interface props {
+  character: RetrievedCharacter;
+}
+export const GuildSelection: React.FC<props> = ({ character }) => {
+  const [loading, setLoading] = useState(true);
+  const [guild, setGuild] = useState<IGuild>();
+  const [newGuild, setNewGuild] = useState<IUnregisteredGuild>();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function updateGuild() {
+      const result = await getGuild(character.name, character.realm);
+      setLoading(false);
+
+      if (!result || result.error) {
+        setError(result?.errorMessage || "Your guild is not registered");
+        return;
+      }
+
+      if (isIGuild(result.data)) {
+        setGuild(result.data);
+      }
+
+      if (isIUnregisteredGuild(result.data)) {
+        setNewGuild(result.data);
+      }
+    }
+    updateGuild();
+  }, []);
+  return (
+    <>
+      {loading && <LoadingSpinner text="Looking for your guild..." />}
+      {guild && (
+        <Card bg="secondary">
+          <Card.Header as="h5">{`${guild.name} - ${guild.realm}`}</Card.Header>
+          <Card.Body>abc</Card.Body>
+        </Card>
+      )}
+      {newGuild && (
+        <>
+          <p>GuildRegistration</p>
+        </>
+      )}
+      {error && (
+        <>
+          <p style={{ textAlign: "center" }}>{error}</p>
+          <p style={{ textAlign: "center" }}>
+            If you are the GM of the guild, click here to register your guild
+          </p>
+        </>
+      )}
+    </>
+  );
+};
