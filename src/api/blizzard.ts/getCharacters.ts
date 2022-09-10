8c278191cb10ce9fa096cb000d13698f9ef32cc6
Metin Ahmad <metinmayi@gmail.com>
2022-09-10 10:35:41 +0200

    Added Docs for getCharacters

--- src/api/blizzard.ts/getCharacters.ts
+++ src/api/blizzard.ts/getCharacters.ts
@@ -1,6 +1,10 @@
 import { GET_CHARS_URL } from "../../config/config";
 import { isRetrievedCharacter } from "../../models/RetrievedCharacter";
 
+/**
+ * This function uses the blizzard authentication to retrieve an accounts max-level characters
+ * @returns {RetrievedCharacter[]} A list of max-level characters that belongs to the account
+ */
 export const getCharacters = async () => {
   try {
     const result = await fetch(GET_CHARS_URL, { credentials: "include" });
