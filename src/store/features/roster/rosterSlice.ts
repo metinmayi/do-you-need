f607e086fce42820e93bf37bb64bd1cd055ed31b
Metin Ahmad <metinmayi@gmail.com>
2022-09-10 21:50:08 +0200

    Prettier changes

--- src/store/features/roster/rosterSlice.ts
+++ src/store/features/roster/rosterSlice.ts
@@ -2,26 +2,26 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IPlayer } from "../../../models/Player";
 
 interface Roster {
-    roster: IPlayer[]
-};
+  roster: IPlayer[];
+}
 
 const initialState: Roster = {
-    roster: []
+  roster: [],
 };
 
 const rosterSlice = createSlice({
-    name: 'roster',
-    initialState,
-    reducers: {
-        setList: (state, action: PayloadAction<IPlayer[]>) => {
-            state.roster = action.payload;
-        },
-        addToList: (state, action: PayloadAction<IPlayer>) => {
-            state.roster.push(action.payload);
-        }
-    }
+  name: "roster",
+  initialState,
+  reducers: {
+    setList: (state, action: PayloadAction<IPlayer[]>) => {
+      state.roster = action.payload;
+    },
+    addToList: (state, action: PayloadAction<IPlayer>) => {
+      state.roster.push(action.payload);
+    },
+  },
 });
 
-export const {setList, addToList} = rosterSlice.actions;
+export const { setList, addToList } = rosterSlice.actions;
 
 export default rosterSlice.reducer;
