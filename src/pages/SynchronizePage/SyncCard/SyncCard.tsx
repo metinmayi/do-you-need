d91a20cf2b02f2609c076cdb8286c9d839465b72
Metin Ahmad <metinmayi@gmail.com>
2022-09-16 00:04:51 +0200

    Added new CSS variables

--- src/pages/SynchronizePage/SyncCard/SyncCard.tsx
+++ src/pages/SynchronizePage/SyncCard/SyncCard.tsx
@@ -1,26 +1,31 @@
-import { Button, Card } from 'react-bootstrap';
-import { BLIZZARD_AUTH_URL } from '../../../config/config';
+import { Button, Card } from "react-bootstrap";
+import { BLIZZARD_AUTH_URL } from "../../../config/config";
 
 export const SyncCard: React.FC = () => {
   return (
     <Card bg="secondary">
-      <Card.Header as="h3">Synchronize</Card.Header>
+      <Card.Header as="h4" style={{ color: "var(--DYNColor)" }}>
+        Synchronize
+      </Card.Header>
       <Card.Body>
         <p>
-          You have to synchronize your Battle.net account in order to browse
-          your connected guilds and to register new ones.
-          <br />
-          <i>
-            You can always synchronize at a later stage throught the settings
-            tab
-          </i>
+          You must synchronize your Battle.net account before you can connect to
+          a guild.
         </p>
-        <Card.Footer className="d-flex justify-content-between">
+        <div className="d-flex justify-content-between">
           <a href={BLIZZARD_AUTH_URL}>
-            <Button variant="success">Synchronize</Button>
+            <Button
+              variant="success"
+              style={{ fontWeight: "var(--buttonWeight)" }}>
+              Synchronize
+            </Button>
           </a>
-          <Button variant="danger">Boss Page</Button>
-        </Card.Footer>
+          <Button
+            variant="danger"
+            style={{ fontWeight: "var(--buttonWeight)" }}>
+            Logout
+          </Button>
+        </div>
       </Card.Body>
     </Card>
   );
