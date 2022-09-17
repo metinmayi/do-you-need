15b263cf208e2b4944a8fdba9929864c7fe08e5d
Metin Ahmad <metinmayi@gmail.com>
2022-09-17 15:21:46 +0200

    Styling changes to CharacterList's StyledUl

--- src/pages/SynchronizePage/SelectionCard/CharacterSelection/CharacterListItem/StyledUl.tsx
+++ src/pages/SynchronizePage/SelectionCard/CharacterSelection/CharacterListItem/StyledUl.tsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const StyledUl = styled.ul`
   list-style-type: none;
-  display: inline-flex;
-  flex-direction: column;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
   gap: 5px;
   li {
     cursor: pointer;
