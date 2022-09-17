c92b003bdd8a390ebaef1e88bd70fd1b996fb0e6
Metin Ahmad <metinmayi@gmail.com>
2022-09-17 15:19:29 +0200

    Removed redundant code

--- src/authentication/syncWithBlizzard/syncWithBlizzard.ts
+++ src/authentication/syncWithBlizzard/syncWithBlizzard.ts
@@ -13,30 +13,4 @@ export const syncWithBlizzard = async (code: string) => {
     `${BASE_URL}blizzard/authenticate`,
     requestOptions
   );
-  console.log(response);
-  //   console.log(code);
-  //   const URL = 'https://eu.battle.net/oauth/token';
-
-  // const BasicAuth = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
-
-  //   const headers = {
-  //     Authorization: `Basic ${CLIENT_ID}:${CLIENT_SECRET}`,
-  //     'Content-Type': 'application/x-www-form-urlencoded'
-  //   }
-
-  //   const params = new URLSearchParams();
-  //   params.append('region', REGION);
-  //   params.append('grant_type', 'authorization_code');
-  //   params.append('code', code);
-  //   params.append('redirect_uri', REDIRECT_URI);
-  //   params.append('client_id', CLIENT_ID);
-
-  //   const requestOptions = {
-  //     method: 'POST',
-  //     body: params,
-  //     headers
-  //   }
-
-  //   const fetchRes = await fetch(URL, requestOptions);
-  //   console.log(fetchRes);
 };
