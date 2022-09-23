f1879fcd43d0ef694d8d83320a6b3f4cef1f3b2c
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-23 22:44:42 +0200

    Added type predicate for IUserGuild

--- src/models/IUserGuild.ts
+++ src/models/IUserGuild.ts
@@ -3,3 +3,11 @@ import { IGuild } from "./IGuild";
 export interface IUserGuild extends IGuild {
   playerRank: string;
 }
+
+export function isIUserGuild(object: any): object is IUserGuild {
+  if (typeof object !== "object") {
+    return false;
+  }
+
+  return typeof object.playerRank === "string";
+}
