72ef9378cc758a3e675b7cada6f671096fafa6b2
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-25 16:23:50 +0200

    Resolved a bug with validating empty arrays

--- src/models/IUserGuild.ts
+++ src/models/IUserGuild.ts
@@ -4,10 +4,13 @@ export interface IUserGuild extends IGuild {
   playerRank: string;
 }
 
-export function isIUserGuild(object: any): object is IUserGuild {
+export function isIUserGuildArray(object: any): object is IUserGuild[] {
   if (typeof object !== "object") {
     return false;
   }
+  if (object.length === 0) {
+    return true;
+  }
 
-  return typeof object.playerRank === "string";
+  return typeof object[0].playerRank === "string";
 }
