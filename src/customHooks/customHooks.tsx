046e38ab17384b0677a0922584408b5bf84d91b9
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-04 15:01:07 +0200

    Removed useMediaQuery

--- src/customHooks/customHooks.tsx
+++ src/customHooks/customHooks.tsx
@@ -1,26 +1,6 @@
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { TypedUseSelectorHook, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
-
-/**
- *
- * @param query Pixels to compare against
- * @returns True if smaller, false if larger
- */
-export const useMediaQuery = (query: number) => {
-  const [isDesktop, setDesktop] = useState(window.innerWidth <= query);
-
-  const updateMedia = () => {
-    setDesktop(window.innerWidth <= query);
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia);
-  });
-  return isDesktop;
-};
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
