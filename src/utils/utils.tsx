4f86f8bed3c980dc71c986972952ab55f0ab0c46
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-04 23:35:43 +0200

    Added functions to convert names between DYN and API

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,4 +1,4 @@
-import { BOSSES, BOSSNAMES } from "../models/Bosses";
+import { BOSSES, BOSSNAMES } from "../models/bosses/bosses";
 
 /**
  * remove later
@@ -16,6 +16,30 @@ const capitalizeFirstLetter = (word: string | boolean) => {
   if (typeof word === "boolean") return;
   return word.slice(0, 1).toUpperCase() + word.slice(1);
 };
+
+/**
+ * Takes a phrase and capitalizes the first letter of each word
+ * @param phrase Word to be converted
+ * @returns {string} The same phrase but every start word is capitalize
+ */
+export function convertToDYNName(phrase: string) {
+  return phrase
+    .split("_")
+    .map((word) => capitalizeFirstLetter(word))
+    .join(" ");
+}
+
+/**
+ * Takes a phrase, lowercases it and joins it with an underscore.
+ * @param phrase Word to be converted
+ * @returns {string} The same phrase but lowercased and joined by an underscore
+ */
+export function convertToServerName(phrase: string) {
+  return phrase
+    .split(" ")
+    .map((word) => word.toLowerCase())
+    .join("_");
+}
 
 /**
  * Checks if the word is of the type BOSSNAME
