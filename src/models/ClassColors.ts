a92ed6bb7f0c77aaccd2a3f6d92d28ce5e185437
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-04 14:57:32 +0200

    Changed ClassColors

---
+++ src/models/ClassColors.ts
@@ -0,0 +1,14 @@
+export const ClassColors: Record<string, string> = {
+  warrior: "#C79C6E",
+  mage: "#69CCF0",
+  paladin: "#F58CBA",
+  hunter: "#ABD473",
+  rogue: "#FFF569",
+  priest: "#FFFFFF",
+  shaman: "#0070DE",
+  warlock: "#9482C9",
+  monk: "#00FF96",
+  druid: "#FF7D0A",
+  demonHunter: "#A330C9",
+  deathKnight: "#C41F3B",
+} as const;
