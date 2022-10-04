3d414b8a44e7cf25cb4d78708c9d26140d47b426
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-04 18:37:42 +0200

    Updated boss names

--- src/models/ICharacterUpgrades.ts
+++ src/models/ICharacterUpgrades.ts
@@ -1,15 +1,15 @@
 import { ICharacterBossUpgrade } from "./ICharacterBossUpgrade";
 
 export class ICharacterUpgrades {
-  vigilant_guardian: ICharacterBossUpgrade[] = [];
-  skolex_the_insatiable_ravener: ICharacterBossUpgrade[] = [];
-  artificer_xymox: ICharacterBossUpgrade[] = [];
-  dausegne_the_fallen_oracle: ICharacterBossUpgrade[] = [];
-  prototype_pantheon: ICharacterBossUpgrade[] = [];
-  lihuvim_principal_architect: ICharacterBossUpgrade[] = [];
-  halondrus_the_reclaimer: ICharacterBossUpgrade[] = [];
-  anduin_wrynn: ICharacterBossUpgrade[] = [];
-  lords_of_dread: ICharacterBossUpgrade[] = [];
-  rygelon: ICharacterBossUpgrade[] = [];
-  the_jailer: ICharacterBossUpgrade[] = [];
+  "Vigilant Guardian": ICharacterBossUpgrade[] = [];
+  "Skolex The Insatiable Ravener": ICharacterBossUpgrade[] = [];
+  "Artificer Xymox": ICharacterBossUpgrade[] = [];
+  "Dausegne The Fallen Oracle": ICharacterBossUpgrade[] = [];
+  "Prototype Pantheon": ICharacterBossUpgrade[] = [];
+  "Lihuvim Principal Architect": ICharacterBossUpgrade[] = [];
+  "Halondrus The Reclaimer": ICharacterBossUpgrade[] = [];
+  "Anduin Wrynn": ICharacterBossUpgrade[] = [];
+  "Lords Of Dread": ICharacterBossUpgrade[] = [];
+  "Rygelon": ICharacterBossUpgrade[] = [];
+  "The Jailer": ICharacterBossUpgrade[] = [];
 }
