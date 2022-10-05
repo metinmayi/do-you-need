a5a09c36e45a198ff50ec52efa02e1b8226c4212
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-05 09:13:23 +0200

    Added bossLoot.ts: bosses indexed with their loot

---
+++ src/models/bosses/SkolexTheInsatiableRavener.ts
@@ -0,0 +1,12 @@
+import { Boss } from "./Boss";
+
+export class SkolexTheInsatiableRavener extends Boss {
+  back = "";
+  shoulder = "";
+  wrist = "";
+  waist = "";
+  trinket = "";
+  one_hand = "";
+  off_hand = "";
+  main_hand = "";
+}
