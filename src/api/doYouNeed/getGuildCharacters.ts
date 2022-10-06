ee951effd332d4799b89aa659023401dcaf11ae5
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-06 22:27:48 +0200

    Added annotation to function

--- src/api/doYouNeed/getGuildCharacters.ts
+++ src/api/doYouNeed/getGuildCharacters.ts
@@ -4,10 +4,10 @@ import vigilant from "../../mock/vigilant_guardian.json";
 import lords from "../../mock/lords_of_dread.json";
 
 /**
- *
- * @param guild
- * @param bossName
- * @returns
+ * Gets all the characters upgrades for a guild from a particular boss.
+ * @param guild Guild to retrieve character's upgrades from.
+ * @param bossName The boss to pull upgrades from
+ * @returns {ICharacterUpgrade[]} Array of characterupgrades
  */
 export function getGuildCharacters(guild: IGuild, bossName: string) {
   if (bossName === "vigilant_guardian") {
