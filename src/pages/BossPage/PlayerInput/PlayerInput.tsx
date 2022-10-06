58fe3ee6b2dbbb5488c1bfac9dba3e0b5e17388a
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-06 20:35:50 +0200

    Cleaned up PlayerInput

--- src/pages/BossPage/PlayerInput/PlayerInput.tsx
+++ src/pages/BossPage/PlayerInput/PlayerInput.tsx
@@ -1,29 +1,10 @@
-import { current } from "@reduxjs/toolkit";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
-import { addPlayerData } from "../../../api/players";
-import {
-  useAppDispatch,
-  useAppSelector,
-} from "../../../customHooks/customHooks";
-import { IPlayer } from "../../../models/Player";
-import { setList } from "../../../store/features/roster/rosterSlice";
 const INPUT_REGEX = /https:\/\/www.raidbots.com\/simbot\/report\/\w+/;
 
 const PlayerInput: React.FC = () => {
   const [input, setInput] = useState("");
   const [validInput, setValidInput] = useState(false);
-
-  const roster = useAppSelector((state) => state.rosterReducer.roster);
-  const dispatch = useAppDispatch();
-
-  async function updateRosterList(e: React.FormEvent) {
-    e.preventDefault();
-    // const receivedPlayer: IPlayer = await addPlayerData(input);
-    // const rosterCopy = structuredClone(roster);
-    // rosterCopy.push(receivedPlayer);
-    // dispatch(setList(rosterCopy));
-  }
 
   useEffect(() => {
     if (INPUT_REGEX.test(input)) {
@@ -34,7 +15,7 @@ const PlayerInput: React.FC = () => {
   }, [input]);
 
   return (
-    <Form className="mb-2 mt-2" onSubmit={(e) => updateRosterList(e)}>
+    <Form className="mb-2 mt-2">
       <InputGroup>
         <FormControl
           placeholder="Insert Raidbots link"
@@ -47,7 +28,7 @@ const PlayerInput: React.FC = () => {
       </InputGroup>
       <Form.Text>
         Sample link:
-        https://www.raidbots.com/simbot/report/uqMnHDXNkUzdixTEdsxLmX
+        <p>https://www.raidbots.com/simbot/report/qU8sUeqm5LUn2f37bmSgAW</p>
       </Form.Text>
     </Form>
   );
