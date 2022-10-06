2d58567c01140b1a1c6102ba129eb61ad21dc73b
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-06 20:33:40 +0200

    Model changes

--- src/models/bosses/VigilantGuardian.ts
+++ src/models/bosses/VigilantGuardian.ts
@@ -1,14 +1,14 @@
 import { Boss } from "./Boss";
 
 export class VigilantGuardian extends Boss {
-  ring: string = "";
-  feet: string = "";
-  legs: string = "";
-  chest: string = "";
-  head: string = "";
-  waist: string = "";
-  hands: string = "";
-  trinket: string = "";
-  one_hand: string = "";
-  off_hand: string = "";
+  ring: string = "-";
+  feet: string = "-";
+  legs: string = "-";
+  chest: string = "-";
+  head: string = "-";
+  waist: string = "-";
+  hands: string = "-";
+  trinket: string = "-";
+  one_hand: string = "-";
+  off_hand: string = "-";
 }
