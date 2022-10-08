4df032cb382a41a99f92902a76b0355fb66c1229
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-08 15:37:11 +0200

    Fixed typo

--- src/customHooks/useHeaderColorListener.ts
+++ src/customHooks/useHeaderColorListener.ts
@@ -3,11 +3,12 @@ import { useEffect } from "react";
 export function useHeaderColorListener() {
   useEffect(() => {
     const th = document.querySelectorAll("thead tr th");
-    const tablehead = document.querySelector("thead");
 
     const observer = new IntersectionObserver(
       ([entry]) => {
-        tablehead?.classList.toggle("stuck", entry.intersectionRatio < 1);
+        th.forEach((header) => {
+          header.classList.toggle("stuck", entry.intersectionRatio < 1);
+        });
       },
       {
         rootMargin: "-1px 0px 0px 0px",
