c11c280bbf7238435e96b9be1d0c2ebe35bc86de
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-08 21:40:48 +0200

    Added RosterList next to the TableComponent

--- src/pages/BossPage/TableComponent/TableComponent.tsx
+++ src/pages/BossPage/TableComponent/TableComponent.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Col, Table } from "react-bootstrap";
 import { TableHead } from "./TableHead";
 import { TableBody } from "./TableBody";
 import { useAppSelector } from "../../../customHooks/useAppSelector";
 import { LoadingSpinner } from "../../../components/LoadingSpinner/LoadingSpinner";
+import { RosterList } from "../RosterList";
 
 const TableComponent: React.FC = () => {
   const roster = useAppSelector((state) => state.rosterReducer.roster);
 
   return roster.length > 0 ? (
-    <Table variant="dark" striped hover>
-      <TableHead />
-      <TableBody />
-    </Table>
+    <>
+      <Col xs={10}>
+        <Table variant="dark" striped hover>
+          <TableHead />
+          <TableBody />
+        </Table>
+      </Col>
+      <Col style={{ maxHeight: "20vh", position: "sticky", top: "0" }}>
+        <RosterList />
+      </Col>
+    </>
   ) : (
     <>
       <Table variant="dark" striped hover>
