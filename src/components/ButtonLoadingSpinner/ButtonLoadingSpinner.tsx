a716c3c31070aa5e6d379d8c7adc05ae7d319cba
Metin Ahmad <metinmayi@gmail.com>
2022-11-09 02:11:33 +0100

    Created a spinner overlay for buttons

---
+++ src/components/ButtonLoadingSpinner/ButtonLoadingSpinner.tsx
@@ -0,0 +1,23 @@
+import spinner from "../assets/images/loadingSpinner.svg";
+interface LoadingSpinnerInterface {
+  children: any;
+}
+
+export const ButtonLoadingSpinner: React.FC<LoadingSpinnerInterface> = ({
+  children,
+}) => {
+  return (
+    <div style={{ position: "relative" }}>
+      <div
+        style={{
+          position: "absolute",
+          height: "100%",
+          transform: "translate(100%)",
+          zIndex: "20",
+        }}>
+        <img src={spinner} alt="loading..." style={{ height: "100%" }}></img>
+      </div>
+      {children}
+    </div>
+  );
+};
--- src/pages/BossPage/CharacterInput/CharacterInput.tsx
+++ src/pages/BossPage/CharacterInput/CharacterInput.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
 import { addCharacterUpgrades } from "../../../api/doYouNeed/addCharacterUpgrades";
+import { ButtonLoadingSpinner } from "../../../components/ButtonLoadingSpinner/ButtonLoadingSpinner";
 import { useAppSelector } from "../../../customHooks/useAppSelector";
 
 const INPUT_REGEX = /https:\/\/www.raidbots.com\/simbot\/report\/\w+/;
@@ -13,6 +14,7 @@ export const CharacterInput: React.FC<CharacterInputProps> = ({
 }) => {
   const [input, setInput] = useState("");
   const [validInput, setValidInput] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const guild = useAppSelector((state) => state.guildReducer);
 
   useEffect(() => {
@@ -24,22 +26,42 @@ export const CharacterInput: React.FC<CharacterInputProps> = ({
   }, [input]);
 
   // Update the roster and update the chara
-  async function submitCharacter(e: React.FormEvent) {
+  async function submitCharacter(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
+    e.currentTarget.blur();
+    setIsLoading(true);
     await addCharacterUpgrades(input, guild);
+    setIsLoading(false);
     setToggleRender((current) => !current);
+    setInput("");
   }
   return (
-    <Form className="mb-2 mt-2" onSubmit={submitCharacter}>
+    <Form className="mb-2 mt-2">
       <InputGroup>
         <FormControl
           placeholder="Insert Raidbots link"
           value={input}
           onChange={(e) => setInput(e.currentTarget.value)}
         />
-        <Button variant="secondary" type="submit" disabled={!validInput}>
-          Add player
-        </Button>
+        <>
+          {isLoading ? (
+            <ButtonLoadingSpinner>
+              <Button
+                variant="secondary"
+                onClick={(e) => submitCharacter(e)}
+                disabled={!validInput}>
+                Add player
+              </Button>
+            </ButtonLoadingSpinner>
+          ) : (
+            <Button
+              variant="secondary"
+              onClick={(e) => submitCharacter(e)}
+              disabled={!validInput}>
+              Add player
+            </Button>
+          )}
+        </>
       </InputGroup>
     </Form>
   );
