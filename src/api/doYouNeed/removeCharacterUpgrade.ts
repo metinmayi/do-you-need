49718c33ce6a4f5bf6937511ac4a52dfa2465723
Metin Ahmad <metinmayi@gmail.com>
2022-11-09 04:14:35 +0100

    Implemented function to delete character upgrades

--- src/api/doYouNeed/removeCharacterUpgrade.ts
+++ src/api/doYouNeed/removeCharacterUpgrade.ts
@@ -1,3 +1,12 @@
+import { DELETE_CHARACTER_UPGRADE } from "../../config/config";
+
 export async function removeCharacterUpgrade(id: string) {
-  // Execute API logic
+  const response = await fetch(DELETE_CHARACTER_UPGRADE, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id }),
+  });
 }
