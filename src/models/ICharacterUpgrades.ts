8f080bf03825ce3194169e1e8cd25f63491abdcc
Metin Ahmad <metinmayi@gmail.com>
2022-11-09 12:28:06 +0100

    Updated ICharacterUpgrade to match new structure

--- src/models/ICharacterUpgrades.ts
+++ src/models/ICharacterUpgrades.ts
@@ -5,21 +5,5 @@ export interface ICharacterUpgrade {
   name: string;
   role: string;
   class: string;
-  upgrades: {
-    head?: string;
-    shoulder?: string;
-    chest?: string;
-    wrist?: string;
-    hands?: string;
-    waist?: string;
-    legs?: string;
-    feet?: string;
-    neck?: string;
-    back?: string;
-    finger?: string;
-    trinket?: string;
-    main_hand?: string;
-    one_hand?: string;
-    off_hand?: string;
-  };
+  upgrades: string[];
 }
